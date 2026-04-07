9f383a4feedb710729692e358c7db9f76fc4921d
Matheus Margraf <20261ctb0100032@estudantes.ifpr.edu.br>
2026-04-07 17:11:05 +0000

    1000py

--- aula-09/1000.py
+++
@@ -1,9 +0,0 @@
-# Estudante: Matheus Margraf
-
-# Data: 24/03/2026
-
-# Problema: 1000 — Hello World!
-
-# Objetivo: Imprimir a mensagem "Hello World!" na saída padrão
-
-print("Hello World!")
--- aula-09/hello.py
+++
@@ -1,5 +0,0 @@
-# Meu primeiro programa em Python
-
-print("Olá, mundo! Meu ambiente está funcionando!")
-
-print("Pronto para programar em Python 🐍")
